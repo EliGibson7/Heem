f87c8c3e2a150cf06ced58756447e482580bf9ac
egibson <egibson5977@W10-5SB6NL3>
2023-05-08 12:41:44 -0500

    new code for the 8th

--- Heem/Main Form.vb
+++ Heem/Main Form.vb
@@ -259,4 +259,8 @@ Public Class frmMain
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles picWheel.Click
 
     End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
